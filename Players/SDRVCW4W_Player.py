c68363e446fbaf3a2aa4430e6fdd95e145049489
Krishna-Saxena <krishnasaxena@outlook.com>
2023-05-20 13:54:11 -0700

    Add permutations

---
+++ Players/SDRVCW4W_Player.py
@@ -0,0 +1,25 @@
+from Uno.UnoEnums import Color, Value
+from Uno.Card import Card
+from Players import CardPriorityPlayer
+
+# Priority:
+# draw 2 > skip > reverse > value > color > wild > wild4
+class SDRVCW4W_Player(CardPriorityPlayer.CardPriorityPlayer):
+    def _get_priority(self, card, top_card, **kwargs):
+        # give cards priority by value as described above
+        if card.value is Value.SKIP:
+            return 0
+        elif card.value is Value.DRAW2:
+            return 1
+        elif card.value is Value.REV:
+            return 2
+        elif type(top_card) is Card and card.value is top_card.value:
+            return 3
+        elif card.color is (top_card.color if type(top_card) is Card else top_card):
+            return 4
+        elif card.value is Value.WILD4:
+            return 5
+        elif card.value is Value.WILD:
+            return 6
+        # this card is unplayable this turn
+        return 10
