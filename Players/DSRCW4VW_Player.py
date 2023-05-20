c33a32b677e60640b5e2ee94406b7bb7ab243b96
krishna-saxena <krishnasaxena@outlook.com>
2023-05-20 12:01:03 -0700

    Players based off of report

--- Players/DSRCW4VW_Player.py
+++ Players/DSRCW4VW_Player.py
@@ -4,22 +4,22 @@ from Players import CardPriorityPlayer
 
 # Priority:
 # draw 2 > skip > reverse > value > color > wild > wild4
-class SRVWDW4C_Player(CardPriorityPlayer.CardPriorityPlayer):
+class DSRCW4VW_Player(CardPriorityPlayer.CardPriorityPlayer):
     def _get_priority(self, card, top_card, **kwargs):
         # give cards priority by value as described above
-        if card.value is Value.SKIP:
+        if card.value is Value.DRAW2:
             return 0
-        elif card.value is Value.REV:
+        elif card.value is Value.SKIP:
             return 1
-        elif type(top_card) is Card and card.value is top_card.value:
+        elif card.value is Value.REV:
             return 2
-        elif card.value is Value.WILD:
-            return 3
-        elif card.value is Value.DRAW2:
-            return 4
-        elif card.value is Value.WILD4:
-            return 5
         elif card.color is (top_card.color if type(top_card) is Card else top_card):
+            return 3
+        elif card.value is Value.WILD4:
+            return 4
+        elif type(top_card) is Card and card.value is top_card.value:
+            return 5
+        elif card.value is Value.WILD:
             return 6
         # this card is unplayable this turn
-        return 100
+        return 10
--- Players/DSRVCW4W_Player.py
+++ Players/DSRVCW4W_Player.py
@@ -4,22 +4,22 @@ from Players import CardPriorityPlayer
 
 # Priority:
 # draw 2 > skip > reverse > value > color > wild > wild4
-class SRCVWDW4_Player(CardPriorityPlayer.CardPriorityPlayer):
+class DSRVCW4W_Player(CardPriorityPlayer.CardPriorityPlayer):
     def _get_priority(self, card, top_card, **kwargs):
         # give cards priority by value as described above
-        if card.value is Value.SKIP:
+        if card.value is Value.DRAW2:
             return 0
-        elif card.value is Value.REV:
+        elif card.value is Value.SKIP:
             return 1
-        elif card.color is (top_card.color if type(top_card) is Card else top_card):
+        elif card.value is Value.REV:
             return 2
         elif type(top_card) is Card and card.value is top_card.value:
             return 3
-        elif card.value is Value.WILD:
+        elif card.color is (top_card.color if type(top_card) is Card else top_card):
             return 4
-        elif card.value is Value.DRAW2:
-            return 5
         elif card.value is Value.WILD4:
+            return 5
+        elif card.value is Value.WILD:
             return 6
         # this card is unplayable this turn
         return 10
